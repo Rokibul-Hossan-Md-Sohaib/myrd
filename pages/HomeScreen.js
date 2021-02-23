6d5014449dcac8b6982bbfac3f10a1cd3fdcbd37
MD ABS Jabed <absj4k@gmail.com>
2021-02-23 17:27:05 +0600

    React Native Local DB with Realm initial commit...

---
+++ pages/HomeScreen.js
@@ -0,0 +1,69 @@
+/*Home Screen With buttons to navigate to diffrent options*/
+import React from 'react';
+import { View } from 'react-native';
+import Mybutton from './components/Mybutton';
+import Mytext from './components/Mytext';
+import Realm from 'realm';
+let realm;
+
+export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    realm = new Realm({
+      path: 'UserDatabase.realm',
+      schema: [
+        {
+          name: 'user_details',
+          properties: {
+            user_id: { type: 'int', default: 0 },
+            user_name: 'string',
+            user_contact: 'string',
+            user_address: 'string',
+          },
+        },
+      ],
+    });
+  }
+  //https://github.com/lawnstarter/react-native-picker-select
+  //https://reactnative.dev/docs/typescript#adding-typescript-to-an-existing-project
+  //https://docs.mongodb.com/realm/react-native/objects/
+  //https://aboutreact.com/example-of-realm-database-in-react-native/
+  //https://stackoverflow.com/questions/50937202/how-to-fast-insert-a-json-into-realm-database-for-a-react-native-app
+
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'white',
+          flexDirection: 'column',
+        }}>
+        <Mytext text="RealM Example" />
+        <Mybutton
+          title="Login"
+          customClick={() => this.props.navigation.navigate('Update')}
+        />
+        <Mybutton
+          title="Register"
+          customClick={() => this.props.navigation.navigate('Register')}
+        />
+        <Mybutton
+          title="Update"
+          customClick={() => this.props.navigation.navigate('Update')}
+        />
+        <Mybutton
+          title="View"
+          customClick={() => this.props.navigation.navigate('View')}
+        />
+        <Mybutton
+          title="View All"
+          customClick={() => this.props.navigation.navigate('ViewAll')}
+        />
+        <Mybutton
+          title="Delete"
+          customClick={() => this.props.navigation.navigate('Delete')}
+        />
+      </View>
+    );
+  }
+}
